da21766f802883867d77470d36e8bb1bcb1ea5c1
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-07-29 13:20:05 -0500

    Add Width

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -81,7 +81,7 @@ function Projects() {
                 <Text fontSize="2xl" fontWeight="bold">
                     What I Built
                 </Text>
-                <SimpleGrid columns={{ base: 1, sm: 1, md: 2 }} spacing={5}>
+                <SimpleGrid columns={{ base: 1, sm: 1, md: 2 }} spacing={5} width="100%">
                     <Box
                         marginTop="1rem"
                         borderRadius="lg"
@@ -214,8 +214,7 @@ function Projects() {
                             alt="phi"
                             width="100%"
                             maxW="500px"
-                            objectFit="contain"
-                            objectPosition="center"
+                           
                         />
                         <Box
                             display={panchangaIsVisible ? "block" : "none"}
