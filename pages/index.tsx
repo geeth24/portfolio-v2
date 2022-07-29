eb5960595e560dcfaec7dbd7ff13c974a6380b12
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-07-29 12:09:01 -0500

    Remove Lazy Load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
-import { Flex, Spinner, useColorModeValue } from "@chakra-ui/react"
+import { useColorModeValue } from "@chakra-ui/react"
 import Head from "next/head"
 import Hero from "../components/Hero/Hero"
-import dynamic from "next/dynamic"
+
 import Navbar from "../components/Navbar/Navbar"
-// import Projects from "../components/Projects/Projects"
-const Projects = dynamic(() => import("../components/Projects/Projects"), {
-    suspense: true,
-})
+import Projects from "../components/Projects/Projects"
 import Skills from "../components/Skills/Skills"
 import Timeline from "../components/Timeline/Timeline"
 
 import { animateScroll } from "react-scroll"
-import { Suspense, useEffect } from "react"
+import { useEffect } from "react"
 import Contact from "../components/Contact/Contact"
 
 const Home = () => {
@@ -25,11 +22,6 @@ const Home = () => {
             }
         }, 5000)
     }, [])
-    const renderLoader = () => (
-        <Flex justifyContent="center" alignItems="center" height="100vh">
-            <Spinner size="xl" thickness="4px" speed="0.65s" color="#347fdb" />
-        </Flex>
-    )
     return (
         <div
             style={{
@@ -43,9 +35,7 @@ const Home = () => {
             <Hero />
             <Timeline />
             <Skills />
-            <Suspense fallback={renderLoader()}>
-                <Projects />
-            </Suspense>
+            <Projects />
             <Contact />
         </div>
     )
